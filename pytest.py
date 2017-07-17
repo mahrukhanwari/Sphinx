b818f34ead64566655671be967a20e33744730ac
Mahrukh Anwari <mahrukhanwariatlasian.com>
2017-07-17 16:42:49 +0500

    edited the python file

--- pytest.py
+++ pytest.py
@@ -1,1 +1,23 @@
+import shutil
+import sys
+
 print "hello, python"
+print ""
+print "printing content file : test"
+with open("test.txt", 'r') as fin:
+    print fin.read()
+print ""	
+print "printing content file : xflow"
+with open("xflow.txt", 'r') as fin:
+    print fin.read()
+print ""	
+print "overwriting content"
+print ""
+with open('test.txt', 'w+') as output, open('xflow.txt', 'r') as input:
+    while True:
+        data = input.read(100000)
+        if data == '':  # end of file reached
+            break
+        output.write(data)
+		
+print "done"
